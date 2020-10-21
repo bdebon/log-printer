7055733426395997e2d724f10917e817ef94b14b
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-10-21 15:38:52 +0200

    Init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = process.env.QOVERY_APPLICATION_LOG_PRINTER_GITHUB_HOSTNAME;
 const port = 3000;
 
 const server = http.createServer((req, res) => {
