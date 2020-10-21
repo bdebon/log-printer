95292e0aa6700673ec802388b7b381a78bea3a91
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-10-21 16:04:18 +0200

    Init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = process.env.QOVERY_APPLICATION_LOG_PRINTER_GITHUB_HOSTNAME;
+const hostname = "0.0.0.0";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -10,6 +10,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
     setInterval(() => console.log(new Date()), 1 * 1000);
 });
