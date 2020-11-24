4794e5325a95a0df70f51bf397001b0a56f937dc
bdebon <benjamin.debon@gmail.com>
2020-11-24 15:45:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require('http');
 const hostname = "0.0.0.0";
 const port = 3000;
 
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
